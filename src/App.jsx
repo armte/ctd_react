8b149dac96957460b4646205a2ad3de48b9611ea
toarms <tommy.armstrong@sas.com>
2025-04-06 16:29:22 -0400

    feat: added TodoListItem component and introduced state to App
    - refactored the list item todos into a TodoList component
    - added a state variable, newTodo, to the App component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
+import { useState } from 'react';
 import './App.css';
 import TodoForm from './TodoForm';
 import TodoList from './TodoList';
 
 function App() {
+  const [newTodo, setNewTodo] = useState('Todo to Complete');
   return (
     <div>
       <h1>My Todos</h1>
+      <p>{newTodo}</p>
       <TodoForm />
       <TodoList />
     </div>
